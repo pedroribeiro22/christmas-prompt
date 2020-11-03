09e363f79c0ad3f908e2b2e005ccb11a1e0f22e4
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-11-03 13:52:42 +0000

    Only executes in december

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ mkdir -p ~/.songs
 
 cp "$PWD/christmas-song.wav" ~/.songs/
 
-echo "0 0 * * * $PWD/howLongUntilChristmas" >> /var/spool/cron/"$(whoami)"
+echo "0 0 * 12 * $PWD/howLongUntilChristmas" >> /var/spool/cron/"$(whoami)"
 
